67a2d91ca9b5e27a5e1207f05ef88ea59f472902
Alex Randaccio <acrandaccio@gmail.com>
2026-05-02 14:54:44 -0400

    add opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,30 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Confuse Them",
   description: "NYC Post-punk",
+
+  openGraph: {
+    title: "Confuse Them",
+    description: "NYC Post-punk",
+    url: "https://confusethem.band",
+    siteName: "Confuse Them",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Confuse Them.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Confuse Them",
+    description: "NYC Post-punk",
+    images: ["/og.png"],
+  },
 };
 
 export default function RootLayout({
